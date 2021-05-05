bce6fe28c83f426b3a3a4b462bfe54ef0be562f3
Tushar1271 <tushar1271.cse18@chitkara.edu.in>
2021-05-05 21:54:06 +0530

    First commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import './style.css';
+export default class SignIn extends React.Component{
+    render() {
+        return <>
+        
+        <div>
+  
+      <h2>SignIn</h2>
+    <label for="username">User Name</label>
+    <input type="text" name="username" placeholder="Enter Username"/>
+<label for="password">Last Name</label>
+    <input type="password" name="password" placeholder="Enter Password"/>
+    
+  
+    <input type="submit" value="SignIn"/>
+    <Link to='/signup'><h2>SignUp</h2></Link>
+
+</div>
+        </>;
+      }
+}
